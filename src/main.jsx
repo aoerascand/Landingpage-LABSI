03753b81a5684f00ed4213b8f6fcb876f676fdcc
aoerascand <iniraditya15@gmail.com>
2026-08-01 09:38:59 +0700

    add gtm

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import TagManager from 'react-gtm-module'
 
 import './index.css'
 import App from './App.jsx'
+
+TagManager.initialize({
+  gtmId: import.meta.env.VITE_GTM_ID,
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
